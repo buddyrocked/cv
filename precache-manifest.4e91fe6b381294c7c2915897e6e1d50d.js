913546dbc2b197c323f68d076c8f08cdc3e5eee8
bhariyana@kemana.com <bhariyana@kemana.com>
2019-07-12 15:58:26 +0700

    Updates

---
+++ precache-manifest.4e91fe6b381294c7c2915897e6e1d50d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7cf462ef169b7e4b65abd12c7f5876",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "ce0ee85398ba86de4c64",
+    "url": "/cv/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cv/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ce0ee85398ba86de4c64",
+    "url": "/cv/static/js/main.3cda5b1c.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
