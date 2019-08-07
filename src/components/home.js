69f72dcec3da288369a43ea0850a003ed516c9f1
bhariyana@kemana.com <bhariyana@kemana.com>
2019-08-07 16:07:13 +0700

    minify son

--- src/components/home.js
+++ src/components/home.js
@@ -22,35 +22,54 @@ class Home extends Component {
         this.props.onFetchHome();
     }
 
-    renderPortfolio(){
-    	let { skills } = this.props.home.content.summary;
-    	return _.map(skills, skill => {
-    		return (
-    			<div className="col-md-3 col-sm-6  fashion logo" key="{ skill.id }">
+    renderSkills() {
+    	console.log(this.props);
+    	/*if(_.isEmpty(this.props.home.content.summary.skills)){
+			return (
+				<div className="col-md-12 col-sm-6  fashion logo">
+	                <a href="single-project.html" className="portfolio_item">
+	                    <img src="http://placehold.it/2000x2000" alt="image-alt" className="img-responsive" />
+	                    <div className="portfolio_item_hover">
+	                        <div className="portfolio-border clearfix">
+	                            <div className="item_info">
+	                                <span>test</span>
+	                                <em>No Data</em>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </a>
+	            </div>
+			);
+		}		
+            
+        return _.map(this.props.home.content.summary.skills, skill => {
+			return (
+				<div className="col-md-3 col-sm-6  fashion logo">
                     <a href="single-project.html" className="portfolio_item">
                         <img src="http://placehold.it/1000x1000" alt="image-alt" className="img-responsive" />
                         <div className="portfolio_item_hover">
                             <div className="portfolio-border clearfix">
                                 <div className="item_info">
-                                    <span>{ skill.language }</span>
-                                    <em>{ skill.status}</em>
+                                    <span>{ this.props.home.title }</span>
+                                    <em>Fashion / Logo</em>
                                 </div>
                             </div>
                         </div>
                     </a>
                 </div>
-    		);
-    	});
-    }
+		  	);
+		});*/
+
+	}
 
 	render() {
-		
+
 		return (
 			<>
 			    <div className="portfolio-div">
 			        <div className="portfolio">
 			            <div className="no-padding portfolio_container">			                
-			                { this.renderPortfolio() }			                
+			                { this.renderSkills() }
 			            </div>
 			        </div>
 			    </div>
